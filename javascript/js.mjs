8d4fcc97413e3617b17bf238d7d845a76cef84f7
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-09 08:40:32 -0700

    Add example from office hours/This is where I was working from to get the example down

---
+++ javascript/js.mjs
@@ -0,0 +1,18 @@
+var favColor = prompt('What is your favorite color?');
+
+favColor = favColor.toLowerCase();
+
+if(favColor === 'purple') {
+    alert('Welcome TJ');
+} else if (favColor === 'yellow') {
+    alert('Welcome Emily!');
+
+} else {
+    name = 'Stranger' ;
+}
+
+alert('Welcome ' + name + '!');
+
+console.log(name);
+console.log(name.length);
+
